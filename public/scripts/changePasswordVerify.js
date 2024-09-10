0473cc7133a6d069c8b04221c9f8f52a9bb56083
arsabot <arie19972012@gmail.com>
2024-09-10 11:06:21 -0300

    aniadi scripts y los quite de los componentes para que este mas limpio modifique las paginas forgot-password y change-password, corregi el estilo en linea

---
+++ public/scripts/changePasswordVerify.js
@@ -0,0 +1,11 @@
+document.getElementById('reset-password-form').addEventListener('submit', function(event) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+      event.preventDefault();
+      document.getElementById('error-message').style.display = 'block';
+    } else {
+      document.getElementById('error-message').style.display = 'none';
+    }
+  });
---
+++ public/scripts/toggleDarkMode.js
@@ -0,0 +1,19 @@
+const toggleDarkMode = () => {
+    document.documentElement.classList.toggle("dark");
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  };
+
+  document
+    .getElementById("themeToggle")
+    .addEventListener("click", toggleDarkMode);
+
+  // Aplicar el tema guardado en localStorage al cargar la página
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark");
+    document.getElementById("themeToggle").checked = true;
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.getElementById("themeToggle").checked = false;
+  }
